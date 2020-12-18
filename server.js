8d3d18d440222edf1c91ca2ee3c82be69ff5d879
User <User@DESKTOP-EOCGBEU>
2020-12-18 18:17:36 +0800

    Testing Login Page

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
-console.log("Hello World");
-console.log("End of the world");
-console.log("test on the vm in laptop");
+const express = require('express');
+const session = require('cookie-session');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.set('view engine','ejs');
+
+app.get('/', (req,res) => {
+	console.log(req.session);
+	if (!req.session.authenticated) {    // user not logged in!
+		res.redirect('/login');
+	} else {
+		res.status(200).render('secrets',{name:req.session.username});
+	}
+});
+
+app.get('/login', (req,res) =>{
+    res.status(200).render('Login',{});
+});
+
+app.listen(process.env.PORT || 8099);
