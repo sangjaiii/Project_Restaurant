fe2e05582578e118576ef0fa813be628d2d5d58e
User <User@DESKTOP-EOCGBEU>
2020-12-18 19:53:01 +0800

    error fix

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const app = express();
 
 app.set('view engine','ejs');
 
+app.use(session({
+    authenticated: false,
+    UserID: null
+}))
+
 app.get('/', (req,res) => {
 	console.log(req.session);
 	if (!req.session.authenticated) {    // user not logged in!
